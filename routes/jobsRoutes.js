a3e2468ba6f9c0739face045ee269cbdbe87844d
ahmadmsaadeh <asssa72412@gmail.com>
2025-04-19 11:08:45 +0300

    Ai parsing User input text to add new job

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
+const uploadMiddleware = require("../middleware/multer");
 
-const {getOrgJobs,getAllJobs,addJob,deleteJob,updateJob} = require('../controllers/jobController')
+const {getOrgJobs,getAllJobs,addJob,deleteJob,updateJob,smartAddJob} = require('../controllers/jobController')
 
 router.get("/getorgjobs",authMiddleware,getOrgJobs);
 router.get("/getalljobs",getAllJobs);
 router.post("/addjob",authMiddleware,addJob);
 router.delete("/deletejob",authMiddleware,deleteJob);
 router.patch("/updatejob",authMiddleware,updateJob);
+router.post("/smart-add-job", authMiddleware, uploadMiddleware.single('file'), smartAddJob);
+
 
 module.exports = router;
