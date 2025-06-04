1f6be76a4b4b9490b733120f844df0fc180fae6f
ahmed-awwad00 <ahmadawwadza7@gmail.com>
2025-06-04 15:05:40 +0300

    freelanceWork

---
+++ models/FreeLance.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const freelancePostSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  content: { type: String, required: true },
+  date: { type: String, required: true },
+});
+
+module.exports = mongoose.model('FreelancePost', freelancePostSchema);
